d4ce7a2d44d9b517b65bbc02ad4399461784b526
shilscode <shils787@gmail.com>
2023-08-18 16:04:13 +0530

    modified

--- 1st.js
+++ 1st.js
@@ -1,1 +1,36 @@
-console.log("Hello world!")
+//console.log("Hello world!")
+//let user= 2+2
+//console.log(user)
+ //let user1= "Shuvendu"
+//console.log(user1)
+
+//let r=5
+//const pi=3.14
+//let area
+
+////area= pi* (r*r)
+//console.log(area)
+// let user1=  "Shuvendu\nShil"
+// console.log(user1)
+
+// let bool= 2<4
+// console.log(typeof bool)
+//logical operator practice 
+
+//  let a = true //1, if false=0
+//  let b =true//1, if false=0
+//  let result = a+b
+//  let res = a-b //true=1 ,1 -1 =0
+//  console.log (result,res) // 2,0
+
+//pre increment & post increment
+
+let a=9
+let b = a++ //4
+let c= ++a //5
+let d= ++b //5
+
+console.log(a,b,c,d) // 11,10,11,10
+
+
+
