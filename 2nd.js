7f57173eb9e8d9986a12ad0ad65a6bbca79a6b00
shilscode <shils787@gmail.com>
2023-08-19 11:04:47 +0530

    2nd day first commit

---
+++ 2nd.js
@@ -0,0 +1,15 @@
+//conditional statement
+let num1 =9;
+let num2=10;
+let num3=19;
+
+if (num1>num2 && num1>num3){
+    console.log(num1 +"  "+"is greater than  3 numbers");
+}
+else if (num2>num3 && num2>num1) {
+    console.log(num2 +"  "+"is greater than  3 numbers");
+    } 
+    
+else {
+    console.log(num3 +"  "+"is greater than  3 numbers");
+ } 
