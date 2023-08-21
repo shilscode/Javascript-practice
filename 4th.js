aae4fe0e569a26d06642d4e8c32d05496a0cacc7
shilscode <shils787@gmail.com>
2023-08-21 12:45:33 +0530

    4th day

---
+++ 4th.js
@@ -0,0 +1,18 @@
+//switch statement
+
+let day= "Sunday"
+
+switch(day){
+    case 'Monday':      
+      console.log("this  is Monday,  You have to  wake up at 7am ")
+      break
+    case 'Tuesday':
+        console.log("this  is Monday,  You have to  wake up at 8am ")
+        break
+    case 'Sunday':
+        console.log("no need to wake up !!!!!!! ")
+        break
+    case 'Friday':
+        console.log("this  is Friday,  partyyyy night!")
+        break
+}
