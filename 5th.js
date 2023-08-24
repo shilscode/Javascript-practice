a5c0c3cbd169b6cc5c8c1fe7583f2654dda49bbc
shilscode <shils787@gmail.com>
2023-08-24 22:44:20 +0530

    5th day

---
+++ 5th.js
@@ -0,0 +1,19 @@
+//Template Literal in JavaScript
+// console.log(`shuvendu
+// shilS`)
+
+// let a =10;
+// let b= 19;
+// console.log(`the value  of a is ${a}  & the value of  b is ${b}`)
+
+
+////loops////////
+
+let n =2;
+
+
+while (n < 3) {
+    console.log("HI")
+  n++;
+  
+}
